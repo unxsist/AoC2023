29d74e55483e7ae72afaa1d4915ab808523cb6c2
Jeroen Nijhuis <jeroen@nijhuis.online>
2023-12-08 12:43:17 +0100

    Committing Day 2/3

---
+++ Day2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MyApp
+{
+    class Game
+    {
+        public int ID;
+        public int highestBlue = 0;
+        public int highestRed = 0;
+        public int highestGreen = 0;
+        public int minimumBlue = 0;
+        public int minimumRed = 0;
+        public int minimumGreen = 0;
+
+        public Game(string rawData)
+        {
+            Regex r_gameId = new Regex("Game ([0-9]*):.*", RegexOptions.Compiled);
+            Regex r_green = new Regex("([0-9]*).green", RegexOptions.Compiled);
+            Regex r_red = new Regex("([0-9]*).red", RegexOptions.Compiled);
+            Regex r_blue = new Regex("([0-9]*).blue", RegexOptions.Compiled);
+            
+            this.ID = int.Parse(r_gameId.Match(rawData).Groups[1].Value);
+
+            var yields = rawData
+                .Replace($"Game {this.ID}:", "")
+                .Split(';', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var yield in yields)
+            {
+                if (r_green.IsMatch(yield))
+                {
+                    int green = int.Parse(r_green.Match(yield).Groups[1].Value);
+                    
+                    this.highestGreen = green > this.highestGreen ? green : this.highestGreen;
+                    this.minimumGreen = (this.minimumGreen < green) ? green : this.minimumGreen;
+                } 
+                
+                if (r_blue.IsMatch(yield))
+                {
+                    int blue = int.Parse(r_blue.Match(yield).Groups[1].Value);
+                    
+                    this.highestBlue = blue > this.highestBlue ? blue : this.highestBlue;
+                    this.minimumBlue = (this.minimumBlue < blue) ? blue : this.minimumBlue;
+                } 
+                
+                if (r_red.IsMatch(yield))
+                {
+                    int red = int.Parse(r_red.Match(yield).Groups[1].Value);
+                    
+                    this.highestRed = red > this.highestRed ? red : this.highestRed;
+                    this.minimumRed = (this.minimumRed < red) ? red : this.minimumRed;
+                } 
+            }
+        }
+    }
+    
+    internal class Program
+    {
+        const int RED_CUBES = 12;
+        const int GREEN_CUBES = 13;
+        const int BLUE_CUBES = 14;
+        
+        static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+        }
+    
+        static List<Game> GetGames()
+        {
+            List<Game> games = new List<Game>();
+    
+            String[] gameData = File.ReadAllLines("Input.txt");
+
+            foreach (var game in gameData)
+            {
+                games.Add(new Game(game));
+            }
+
+            return games;
+        }
+
+        static void Part1()
+        {
+            var sum = 0; 
+            
+            foreach (var game in GetGames()) 
+            {
+                if (game.highestRed <= RED_CUBES && game.highestGreen <= GREEN_CUBES && game.highestBlue <= BLUE_CUBES)
+                {
+                    sum += game.ID;
+                }    
+            }
+            
+            Console.WriteLine($"Sum of possible games: {sum}");
+        }
+
+        static void Part2()
+        {
+            var sum = 0;
+
+            foreach (var game in GetGames())
+            {
+                sum += (game.minimumRed * game.minimumBlue * game.minimumGreen);
+            }
+            
+            Console.WriteLine($"Sum of power of minimum cubes: {sum}");
+        }
+    }
+}
---
+++ Day3/Program.cs
@@ -0,0 +1,141 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+        }
+
+        static void Part1()
+        {
+            var lines = File.ReadAllLines("Input.txt");
+            Console.WriteLine("Sum of part no's: " + SumPartNumbers(lines));
+        }
+
+        static void Part2()
+        {
+            var lines = File.ReadAllLines("Input.txt");
+            Console.WriteLine("Sum of gear ratios: " + SumGearRatios(lines));
+        }
+        
+        static int SumPartNumbers(string[] schematic)
+        {
+            HashSet<string> countedNumbers = new HashSet<string>();
+            int sum = 0;
+
+            for (int y = 0; y < schematic.Length; y++)
+            {
+                for (int x = 0; x < schematic[y].Length; x++)
+                {
+                    if (IsSymbol(schematic[y][x]))
+                    {
+                        sum += SumAdjacentNumbers(schematic, x, y, countedNumbers);
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        static int SumAdjacentNumbers(string[] schematic, int x, int y, HashSet<string> countedNumbers)
+        {
+            int sum = 0;
+            for (int dy = -1; dy <= 1; dy++)
+            {
+                for (int dx = -1; dx <= 1; dx++)
+                {
+                    if (dy == 0 && dx == 0) continue; // Skip the symbol itself
+
+                    int adjY = y + dy;
+                    int adjX = x + dx;
+
+                    if (adjY >= 0 && adjY < schematic.Length && adjX >= 0 && adjX < schematic[adjY].Length)
+                    {
+                        if (char.IsDigit(schematic[adjY][adjX]) && !countedNumbers.Contains($"{adjY},{adjX}"))
+                        {
+                            sum += ExtractAndMarkNumber(schematic, adjX, adjY, countedNumbers);
+                        }
+                    }
+                }
+            }
+            return sum;
+        }
+
+        static int ExtractAndMarkNumber(string[] schematic, int x, int y, HashSet<string> countedNumbers)
+        {
+            string number = "";
+            int initialX = x;
+            while (x < schematic[y].Length && char.IsDigit(schematic[y][x]))
+            {
+                number += schematic[y][x];
+                countedNumbers.Add($"{y},{x}");
+                x++;
+            }
+
+            // Check if the number extends to the left of the initial digit
+            x = initialX - 1;
+            while (x >= 0 && char.IsDigit(schematic[y][x]))
+            {
+                number = schematic[y][x] + number;
+                countedNumbers.Add($"{y},{x}");
+                x--;
+            }
+
+            return int.TryParse(number, out int result) ? result : 0;
+        }
+        
+        static int SumGearRatios(string[] schematic)
+        {
+            int sum = 0;
+            HashSet<string> countedNumbers = new HashSet<string>();
+
+            for (int y = 0; y < schematic.Length; y++)
+            {
+                for (int x = 0; x < schematic[y].Length; x++)
+                {
+                    if (schematic[y][x] == '*')
+                    {
+                        var adjacentNumbers = FindAdjacentNumbers(schematic, x, y, countedNumbers);
+                        if (adjacentNumbers.Count == 2)
+                        {
+                            sum += adjacentNumbers[0] * adjacentNumbers[1];
+                        }
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        static List<int> FindAdjacentNumbers(string[] schematic, int x, int y, HashSet<string> countedNumbers)
+        {
+            List<int> numbers = new List<int>();
+            for (int dy = -1; dy <= 1; dy++)
+            {
+                for (int dx = -1; dx <= 1; dx++)
+                {
+                    if (dy == 0 && dx == 0) continue;
+
+                    int adjY = y + dy;
+                    int adjX = x + dx;
+
+                    if (adjY >= 0 && adjY < schematic.Length && adjX >= 0 && adjX < schematic[adjY].Length)
+                    {
+                        if (char.IsDigit(schematic[adjY][adjX]) && !countedNumbers.Contains($"{adjY},{adjX}"))
+                        {
+                            numbers.Add(ExtractAndMarkNumber(schematic, adjX, adjY, countedNumbers));
+                        }
+                    }
+                }
+            }
+            return numbers;
+        }
+
+        static bool IsSymbol(char ch)
+        {
+            return !char.IsDigit(ch) && ch != '.';
+        }
+    }
+}
